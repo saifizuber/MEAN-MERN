7c79ffb60f0d6a8cf7f4a89d47223b42d6eb9ced
Mohd Zuber <119002523+saifizuber@users.noreply.github.com>
2023-02-20 22:11:00 +0530

    Add files via upload

---
+++ mean mern/list.js
@@ -0,0 +1,6 @@
+var arr1 =['batman','robin'];
+var arr2 =['joker','bane'];
+var comp = arr1.concat(arr2);
+
+
+['batman','robin','joker','bane']
---
+++ mean mern/prime.js
@@ -0,0 +1,42 @@
+var prime = function (){
+    var num;
+    for (num = 0; num < 101; num++){
+        if (num % 2 === 0){
+            break;
+        }
+        else if (num % 3 === 0){
+            break;
+        }
+        else if (num % 4=== 0){
+            break;
+        }
+        else if (num % 5 === 0){
+            break;
+        }
+        else if (num % 6 === 0){
+            break;
+        }
+        else if (num % 7 === 0){
+            break;
+        }
+        else if (num % 8 === 0){
+            break;
+        }
+        else if (num % 9 === 0){
+            break;
+        }
+        else if (num % 10 === 0){
+            break;
+        }
+        else if (num % 11 === 0){
+            break;
+        }
+        else if (num % 12 === 0){
+            break;
+        }
+        else {
+            return num;
+        }
+    }
+    };
+    console.log(prime());
